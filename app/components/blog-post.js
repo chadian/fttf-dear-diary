335566e8d348065cc2016d746e421bf8752b368f
Chad Carbert <chadcarbert@me.com>
2017-01-14 15:55:07 -0800

    Adds markdown and pretty dates to blog-post

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
   rawTitle: null,
   rawDate: null,
-  rawContent: null
+  rawContent: null,
+
+  title: Ember.computed.readOnly('rawTitle'),
+  date: Ember.computed('rawDate', function() {
+    return moment(this.get('rawDate'), "YYYY-MM-DD").format('LL');
+  })
 });
