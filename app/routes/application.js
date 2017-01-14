b3ec6ce4a23d806bf26cd9b433c8cefcf6011a6e
Chad Carbert <chadcarbert@me.com>
2017-01-14 14:42:03 -0800

    Retrieve blog posts from shoebox in model hook and pass to blog-post component

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
 import fetch from "ember-network/fetch";
+const { RSVP } = Ember;
 
 export default Ember.Route.extend({
+  fastboot: Ember.inject.service(),
+
   model() {
-    return fetch('https://api.github.com/users/chadian')
-      .then(function(response) {
-        return response.json();
-      });
+    const avatarUrl = fetch('https://api.github.com/users/chadian')
+      .then(response => response.json())
+      .then(json => json.avatar_url);
+
+    const blogPosts = this.get('fastboot.shoebox').retrieve('blog-posts');
+
+    return RSVP.hash({
+      avatarUrl: avatarUrl,
+      blogPosts: blogPosts
+    });
   }
 });
