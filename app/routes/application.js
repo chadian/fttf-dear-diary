c9a6587827a42e46647045e560dba769a355f873
Chad Carbert <chadcarbert@me.com>
2017-01-16 17:41:58 -0800

    Moves pulling of blog posts from instance initializer to application route and model hook
    Fixes issue where blog posts weren't in FastBoot render, but were in the shoebox, and
    therefore loaded on the browser
    
    deferRendering on the fastboot service wasn't working to defer until the
    blog posts were retrieved and in the shoebox before the application route
    model hook was called. This makes sense as the route is not responsible for
    rendering, but fastboot would still continue to render the application controller
    template without the shoebox loaded. The shoebox would be loaded before the
    response was sent to the browser however.

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,15 +6,52 @@ export default Ember.Route.extend({
   fastboot: Ember.inject.service(),
 
   model() {
+    const fastboot = this.get('fastboot');
+
+    let blogPosts;
+    if (fastboot.get('isFastBoot')) {
+      blogPosts = this.pullBlogPosts();
+      blogPosts.then(posts => this.get('fastboot.shoebox').put('blog-posts', posts));
+    } else {
+      blogPosts = RSVP.resolve(this.get('fastboot.shoebox').retrieve('blog-posts'));
+    }
+
     const avatarUrl = fetch('https://api.github.com/users/chadian')
       .then(response => response.json())
       .then(json => json.avatar_url);
-
-    const blogPosts = this.get('fastboot.shoebox').retrieve('blog-posts');
 
     return RSVP.hash({
       avatarUrl: avatarUrl,
       blogPosts: blogPosts
     });
+  },
+
+  pullBlogPosts() {
+    const MongoClient = FastBoot.require('mongodb').MongoClient;
+    const mongoUrl = 'mongodb://localhost:27017/dear-diary';
+
+    const pullBlogPosts = new RSVP.Promise((resolve, reject) => {
+      MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) {
+          db.close();
+          reject('Unable to connect to MongoDB');
+          return;
+        }
+
+        const postsCollection = db.collection('blog-posts');
+        postsCollection.find({}).toArray(function(err, posts) {
+          db.close();
+
+          if (err) {
+            reject('Unable to find blog-posts in MongoDB');
+            return;
+          }
+
+          resolve(posts);
+        });
+      });
+    });
+
+    return pullBlogPosts;
   }
 });
